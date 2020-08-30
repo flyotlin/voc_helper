c3cdd0b86ba8b130e15bbaf04df33c0c916987c0
flyotlin <flyotlin@gmail.com>
2020-08-30 23:06:00 +0800

    Feat: create the basic view of voc cards

--- App.tsx
+++ App.tsx
@@ -2,10 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+export interface Props {
+  vocabulary: String;
+  definition: String;
+};
+
+export function Card(props: Props) {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.voc}>{props.vocabulary}</Text>
+      <Text style={styles.def}>{props.definition}</Text>
+    </View>
+  );
+}
+
+const words = [["と思います", "猜想、覺得、認為"], ["覚える", "察覺"], ["Simulate", "模擬"]];
+
 export default function App() {
+  let cards = words.map( (word) => {
+    return (
+      <Card 
+        key={word[0].toString()}
+        vocabulary={word[0]}
+        definition={word[1]}
+      />
+    );
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {cards}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +43,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card: {
+    borderColor: 'black',
+    borderWidth: 3,
+  },
+  voc: {
+    fontSize: 35,
+    fontWeight: "700",
+    textAlign: "center",
+  }, 
+  def: {
+    fontSize: 20,
+    fontWeight: "normal",
+    textAlign: "center",
   },
 });
